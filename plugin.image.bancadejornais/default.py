dc663e5cea8a32eead641f1fbfdbc37d5fd6c10a
Daniel Mota <leinad4mind@gmail.com>
2017-03-13 13:20:32 +0000

    Correcção para Sapo24

--- plugin.image.bancadejornais/default.py
+++ plugin.image.bancadejornais/default.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-# Copyright 2013 enen92 
+# Copyright 2013~2017 enen92 & Leinad4Mind
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -21,28 +21,29 @@ addon_id = 'plugin.image.bancadejornais'
 selfAddon = xbmcaddon.Addon(id=addon_id)
 addonfolder = selfAddon.getAddonInfo('path')
 artfolder = '/resources/icons/'
-
+sitio = 'http://24.sapo.pt/jornais'
 
 
 def CATEGORIES():
-	addDir('[B]Nacional[/B]','http://noticias.sapo.pt/banca/nacional/',1, addonfolder + artfolder + 'nacional.png')
-	addDir('[B]Desporto[/B]','http://noticias.sapo.pt/banca/desporto/',1, addonfolder + artfolder + 'desporto.png')
-	addDir('[B]Economia[/B]','http://noticias.sapo.pt/banca/economia/',1, addonfolder + artfolder + 'economia.png')
-	addDir('[B]Tecnologia[/B]','http://noticias.sapo.pt/banca/tecnologia/',1, addonfolder + artfolder + 'tecnologia.png')
-	addDir('[B]Local[/B]','http://noticias.sapo.pt/banca/local/',1, addonfolder + artfolder + 'local.png')
-	addDir('[B]Internacional[/B]','http://noticias.sapo.pt/banca/internacional/',1, addonfolder + artfolder + 'internacional.png')
-	addDir('[B]Revistas[/B]','http://noticias.sapo.pt/banca/revistas/',1, addonfolder + artfolder + 'revistas.png')
+	addDir('[B]Nacional[/B]',sitio+'/nacional/',1, addonfolder + artfolder + 'nacional.png')
+	addDir('[B]Desporto[/B]',sitio+'/desporto/',1, addonfolder + artfolder + 'desporto.png')
+	addDir('[B]Economia[/B]',sitio+'/economia/',1, addonfolder + artfolder + 'economia.png')
+	addDir('[B]Local[/B]',sitio+'/local/',1, addonfolder + artfolder + 'local.png')
+	addDir('[B]Lusofonia[/B]',sitio+'/lusofonia/',1, addonfolder + artfolder + 'local.png')
+	addDir('[B]Internacional[/B]',sitio+'/internacional/',1, addonfolder + artfolder + 'internacional.png')
+	addDir('[B]Revistas[/B]',sitio+'/revistas/',1, addonfolder + artfolder + 'revistas.png')
 		
 		
 		
 
 def jornal_list(url):
 	link = abrir_url(url)
-	match=re.compile('imgsrc="http://thumbs.web.sapo.io/\?pic=(.+?)&.+?" imgalt="(.+?)"').findall(link)
+	match=re.compile('img data-src="(//thumbs.web.sapo.io/\?epic=.+?)(&.+?)" src=".+?" alt="(.+?)"').findall(link)
 	totalitems = len(match)
-	for thumbnail,titulo in match:
-			thumbnail = urllib.unquote(thumbnail)
-			addLink('[B]' + titulo + '[/B]',thumbnail,thumbnail,totalitems)
+	for img,thumbnail,titulo in match:
+			imagem = urllib.unquote('http:'+img+'&W=1520&H=0&delay_optim=1&tv=1&crop=center')
+			thumbs = urllib.unquote('http:'+img+'&W=520&H=0&delay_optim=1&tv=1&crop=center')
+			addLink('[B]' + titulo + '[/B]',imagem,thumbs,totalitems)
 	xbmc.executebuiltin("Container.SetViewMode(500)")
 
 
@@ -57,31 +58,31 @@ def abrir_url(url):
 	response.close()
 	return link
 
-                
+				
 def get_params():
-        param=[]
-        paramstring=sys.argv[2]
-        if len(paramstring)>=2:
-                params=sys.argv[2]
-                cleanedparams=params.replace('?','')
-                if (params[len(params)-1]=='/'):
-                        params=params[0:len(params)-2]
-                pairsofparams=cleanedparams.split('&')
-                param={}
-                for i in range(len(pairsofparams)):
-                        splitparams={}
-                        splitparams=pairsofparams[i].split('=')
-                        if (len(splitparams))==2:
-                                param[splitparams[0]]=splitparams[1]
-                                
-        return param
+		param=[]
+		paramstring=sys.argv[2]
+		if len(paramstring)>=2:
+				params=sys.argv[2]
+				cleanedparams=params.replace('?','')
+				if (params[len(params)-1]=='/'):
+						params=params[0:len(params)-2]
+				pairsofparams=cleanedparams.split('&')
+				param={}
+				for i in range(len(pairsofparams)):
+						splitparams={}
+						splitparams=pairsofparams[i].split('=')
+						if (len(splitparams))==2:
+								param[splitparams[0]]=splitparams[1]
+								
+		return param
 
 
 
 
-def addLink(name,url,iconimage,number_of_items):
+def addLink(name,url,thumbs,number_of_items):
 	ok=True
-	liz=xbmcgui.ListItem(name, iconImage="DefaultImage.png", thumbnailImage=iconimage)
+	liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=thumbs)
 	liz.setProperty('fanart_image', addonfolder + artfolder + 'fanart.jpg')
 	liz.setInfo( type='image', infoLabels={ "Title": name } )
 	ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=liz,totalItems=number_of_items)
@@ -96,39 +97,38 @@ def addDir(name,url,mode,iconimage):
 	liz.setInfo( type="Video", infoLabels={ "Title": name })
 	ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
 	return ok
-        
-              
+		
+			  
 params=get_params()
 url=None
 name=None
 mode=None
 
 try:
-        url=urllib.unquote_plus(params["url"])
+		url=urllib.unquote_plus(params["url"])
 except:
-        pass
+		pass
 try:
-        name=urllib.unquote_plus(params["name"])
+		name=urllib.unquote_plus(params["name"])
 except:
-        pass
+		pass
 try:
-        mode=int(params["mode"])
+		mode=int(params["mode"])
 except:
-        pass
+		pass
 
 print "Mode: "+str(mode)
 print "URL: "+str(url)
 print "Name: "+str(name)
 
 if mode==None or url==None or len(url)<1:
-        print ""
-        CATEGORIES()
-       
-       
+		print ""
+		CATEGORIES()
+	   
+	   
 elif mode==1:
-        print ""
-        jornal_list(url)
-
+		print ""
+		jornal_list(url)
 
 
 xbmcplugin.endOfDirectory(int(sys.argv[1]))
